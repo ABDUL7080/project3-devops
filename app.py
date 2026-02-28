c7640395d45995d5637de1036c53dc9224c597f5
Abdul Rahman <abdrah3000gmail.com>
2026-02-28 05:40:15 +0530

    Trigger CI/CD via webhook

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 @app.route("/")
 def home():
     logging.info("Homepage visited")
-    return "🚀 Project 3 - System Info App details Abdul  shaikh"
+    return "🚀 Project 3 - System Info App details Abdull  shaikh"
 
 @app.route("/info")
 def info():
